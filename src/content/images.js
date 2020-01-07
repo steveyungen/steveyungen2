bfc62d14e73d5f52dea0f7912a5aae8cc40484c7
steveyungen <59627079+steveyungen@users.noreply.github.com>
2020-01-07 14:30:32 -0800

    initial commit

---
+++ src/content/images.js
@@ -0,0 +1,128 @@
+import ZeroAndSlicks from "../images/steve/zero-and-the-slicks.jpg";
+import GenericBand1 from "../images/steve/the-generic-band.jpg";
+import GenericBand2 from "../images/steve/the-generic-band-1986.jpg";
+import GenericBand3 from "../images/steve/red-shirt-band.jpg";
+import LeiAloha from "../images/steve/the-lei-aloha-serenaders.jpg";
+import TurksSteve from "../images/steve/steve-turks.jpg";
+import GrandSteve from "../images/steve/steve-grand-canyon-guitar.jpg";
+import LapSteve from "../images/steve/lap-steve.jpg";
+import Steve1986 from "../images/steve/steve-1986.jpg";
+import SteveGreg from "../images/steve/steve-greg.jpg";
+import StevePili from "../images/steve/steve-pili.jpg";
+import SteveSOSAChild from "../images/steve/steve-sosa-child.jpg";
+import SteveCurtis from "../images/steve/steve-and-curtis.jpg";
+import SteveGuitars from "../images/steve/steve-guitars.jpg";
+import SteveTaylors2 from "../images/steve/steve-taylor-2.jpg";
+import Route66 from "../images/steve/steve-route-66.jpg";
+import SteveUkulele from "../images/steve/steve-ukulele.jpg";
+import Windmill from "../images/steve/steve-windmill.jpg";
+
+export const galleryImages = {
+  zeroAndSlicks: {
+    image: ZeroAndSlicks,
+    title: "Zero and the Slicks",
+    description: "1973"
+  },
+
+  genericBand1: {
+    image: GenericBand1,
+    title: "The Generic Band",
+    description: "Nyssa, OR, 1985"
+  },
+
+  genericBand2: {
+    image: GenericBand2,
+    title: "The Generic Band",
+    description: "Nyssa, OR, 1986"
+  },
+
+  genericBand3: {
+    image: GenericBand3,
+    title: "The Generic Band",
+    description: ""
+  },
+
+  leiAloha: {
+    image: LeiAloha,
+    title: "The Lei Aloha Serenaders, 2009",
+    description: "Left to Right: Pili Moreno, Kumu Pekelo Day, Steve Yungen"
+  },
+
+  turksSteve: {
+    image: TurksSteve,
+    title: "Turks Caicos",
+    description: "2017"
+  },
+
+  grandSteve: {
+    image: GrandSteve,
+    title: "Steve Next to a Large Pit",
+    description: "March 2017"
+  },
+
+  steveUkulele: {
+    image: SteveUkulele,
+    title: "Steve Warming Up Pre-Show",
+    description: "2018"
+  },
+
+  steveGreg: {
+    image: SteveGreg,
+    title: "Steve and Greg",
+    description: ""
+  },
+
+  route66: {
+    image: Route66,
+    title: "Route 66",
+    description: "Album cover material?"
+  },
+
+  steveGuitars: {
+    image: SteveGuitars,
+    title: "Steve's Taylors",
+    description: ""
+  },
+
+  stevePili: {
+    image: StevePili,
+    title: "Steve and Pili",
+    description: ""
+  },
+
+  steveSOSAChild: {
+    image: SteveSOSAChild,
+    title: "Jacksonville Farmer's Market",
+    description: "Southern Oregon Songwriters, 2019"
+  },
+
+  steveCurtis: {
+    image: SteveCurtis,
+    title: "Steve and Curtis",
+    description: "Jacksonville Farmer's Market, 2019"
+  },
+
+  lapSteve: {
+    image: LapSteve,
+    title: "Steve with Lap Steel",
+    description: "Florence, OR, 2015"
+  },
+
+  steve1986: {
+    image: Steve1986,
+    title: "Steve Shredding",
+    description: "1986"
+  },
+
+  steveTaylors2: {
+    image: SteveTaylors2,
+    title: "Steve's Taylors",
+    description: ""
+  },
+
+  windmill: {
+    image: Windmill,
+    title: "(Windmilling) at the beach",
+    description: "Oregon coast"
+  }
+};
---
+++ src/content/songs.js
@@ -0,0 +1,59 @@
+export const DEFAULT_SONG = "aloha-week-hula";
+
+export const hawaiian = {
+  "Aloha Week Hula": "aloha-week-hula",
+  "Blue Hawaiian Moonlight": "blue-hawaiian-moonlight",
+  "Hanalei Moon": "hanalei-moon",
+  "Hi'ilawe": "hiilawe",
+  "Hula Breeze": "hula-breeze",
+  Kainoa: "kainoa",
+  "Koke'e": "kokee",
+  "Mauna Chimes": "mauna-chimes",
+  Pohakuloa: "pohakuloa",
+  Seabird: "seabird",
+  Ulupalakua: "ulupalakua",
+  Waikiki: "waikiki",
+  "White Sandy Beach": "white-sandy-beach-1"
+};
+
+export const composed = {
+  "At The Beach": "at-the-beach",
+  "At The Beach v. 2": "at-the-beach-1",
+  "A Change of Heart": "a-change-of-heart-aug-2017",
+  "Another Sunny Day": "03-another-sunny-day",
+  Domino: "domino-170",
+  "Flying Harmonicas": "flying-harmonicas-1",
+  "Got to Move Forward": "05-got-to-move-forward",
+  "In This Light You Look Like a Friend of Mine":
+    "in-this-light-you-look-like-a-friend-of-mine",
+  "Old Folks Tell Their Stories": "the-old-folks-tell-their-stories",
+  "Picnic v. 1": "01-picnic",
+  "Sprayed By Skunk v. 3": "sprayed-by-skunk-3",
+  "Sprayed By Skunk (Lap Steel)":
+    "sprayed-by-skunk-acoustic-version-w-lap-steel",
+  "The Face is Familiar": "04-the-face-is-familiar",
+  "The Nomads": "07-the-nomads",
+  "The Rain v. 5": "the-rain-5"
+};
+
+export const covers = {
+  "All My Loving": "all-my-loving",
+  "Between the Devil and the Deep Blue Sea":
+    "between-the-devil-and-the-deep-blue-sea",
+  "City of Stars": "city-of-stars-steve",
+  "I Fall to Pieces": "i-fall-to-pieces",
+  "Moonlight Lady": "moonlight-lady-final-5-30-16",
+  "Red Sails In the Sunset": "red-sails-in-the-sunset",
+  "Slow Down": "slow-down",
+  "Sunshine On My Shoulders": "sunshine-on-my-shoulders",
+  "Sweet Child of Mine (Steel Guitar)": "sweet-child-of-mine-steel",
+  "Thank You Girl": "thank-you-girl",
+  "The Warmth of the Sun": "the-warmth-of-the-sun",
+  "Travelin' Man": "travelin-man",
+  "Will You Still Love Me Tomorrow (Dance Version)":
+    "will-you-still-love-me-tomorrow-dance-version"
+};
+
+export const collabs = {
+  "If I Fell": "if-i-fell-greg-and-steve-10-2-16"
+};
