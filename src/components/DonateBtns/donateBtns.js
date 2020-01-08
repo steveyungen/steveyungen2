df76036abfb421a06af3e2becc4aa6c8c81cead8
Curtis Yungen <curtisyungen@gmail.com>
2020-01-07 20:06:58 -0800

    update paypal biz id

--- src/components/DonateBtns/donateBtns.js
+++ src/components/DonateBtns/donateBtns.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./donateBtns.css";
 
 const PATREON_ID = "29073055";
-const PAYPAL_BIZ_ID = "NAY72ENRLHF6L";
+const PAYPAL_BIZ_ID = "NCZH4FUURXPXY";
 
 class DonateBtns extends Component {
   render() {
