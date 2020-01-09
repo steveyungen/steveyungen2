4cfefba36af4f6064044ddbd672cd376ad018fad
steveyungen <59627079+steveyungen@users.noreply.github.com>
2020-01-08 21:49:29 -0800

    Update Gallery.js

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -11,6 +11,12 @@ const photoStyle = {
 };
 
 class Gallery extends Component {
+  componentDidMount = () => {
+    if (window.location.protocol !== 'https:') {
+        window.location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
+    }
+  }
+  
   render() {
     return (
       <div className="container gallery-container">
