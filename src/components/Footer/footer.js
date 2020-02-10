245cf5f1b45eb6a42ab2f6b52d8bf6db9ac70311
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2020-02-10 08:46:57 -0800

    add closing tag

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -11,7 +11,12 @@ class Footer extends Component {
           <SocialIcons />
           <ScrollTop />
           <a href="https://www.hitwebcounter.com" target="_blank">
-            <img src="https://hitwebcounter.com/counter/counter.php?page=7193407&style=0007&nbdigits=5&type=page&initCount=0" title="Free-Counter" Alt="hitwebcounter"   border="0" >
+            <img 
+              src="https://hitwebcounter.com/counter/counter.php?page=7193407&style=0007&nbdigits=5&type=page&initCount=0" 
+              title="Free-Counter" 
+              Alt="hitwebcounter"   
+              border="0" 
+            />
           </a>           
           <div className="copyright">
             <div>Home page cover photo taken by Karen Irmscher.</div>
