2f782a0ae7bad26bfca4ad7c187dbfc7ea3699c3
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2020-02-13 13:48:30 -0800

    remove hit counter

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -10,16 +10,6 @@ class Footer extends Component {
         <div className="footer-inner">
           <SocialIcons />
           <ScrollTop />
-          <div className="hitCounter">
-            <a href="https://www.hitwebcounter.com" target="_blank">
-              <img 
-                src="https://hitwebcounter.com/counter/counter.php?page=7193407&style=0007&nbdigits=5&type=page&initCount=0" 
-                title="Free-Counter" 
-                Alt="hitwebcounter"   
-                border="0" 
-              />
-            </a>    
-          </div>
           <div className="copyright">
             <div>Home page cover photo taken by Karen Irmscher.</div>
             <div>Website designed and built by <a href="https://curtisyungen.github.io/Portfolio/" target="_blank" rel="noopener noreferrer">Curtis Yungen</a>.</div>
