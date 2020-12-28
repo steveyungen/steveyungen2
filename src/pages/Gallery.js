cbd4a3080afc41ec9c63af5d94198f7b35098c73
Curtis Yungen <curtisyungen@gmail.com>
2020-12-27 21:59:51 -0800

    update gallery tagline

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -17,7 +17,7 @@ class Gallery extends Component {
         <div className="row row-style justify-content-center text-center">
           <p className="title large-title">PHOTOS</p>
           <p className="description">
-            The pictures and videos in this gallery are from different musical
+            The pictures in this gallery are from different musical
             periods in my life. They’ve all had their fun times!
           </p>
           <div className="spacer" />
