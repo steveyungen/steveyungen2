4b7ec04281de845634f12d7e198d369a566c4dc8
Curtis Yungen <curtisyungen@gmail.com>
2021-01-18 21:42:31 -0600

    remove dead links in songs

--- src/content/songs.js
+++ src/content/songs.js
@@ -2,14 +2,12 @@ export const DEFAULT_SONG = "at-the-beach";
 
 export const hawaiian = {
   "Aloha Week Hula": "aloha-week-hula",
-  "Hi'ilawe": "hiilawe",
   "Hula Breeze": "hula-breeze",
   Kainoa: "kainoa",
   "Koke'e": "kokee",
   "Mauna Chimes": "mauna-chimes",
   Pohakuloa: "pohakuloa",
   Seabird: "seabird",
-  Ulupalakua: "ulupalakua",
   "White Sandy Beach": "white-sandy-beach-1",
 };
 
@@ -17,26 +15,16 @@ export const originals = {
   "At The Beach": "at-the-beach",
   "At The Beach v. 2": "at-the-beach-1",
   "A Change of Heart": "a-change-of-heart-aug-2017",
-  "Another Sunny Day": "03-another-sunny-day",
-  "???Bananas": "bananas",
   Domino: "domino-170",
   "Flying Harmonicas": "flying-harmonicas-1",
-  "Got to Move Forward": "05-got-to-move-forward",
   "In This Light You Look Like a Friend of Mine":
     "in-this-light-you-look-like-a-friend-of-mine",
-  "Old Folks Tell Their Stories": "the-old-folks-tell-their-stories",
-  "Picnic v. 1": "01-picnic",
   "Sprayed By Skunk v. 3": "sprayed-by-skunk-3",
-  "Sprayed By Skunk (Lap Steel)":
-    "sprayed-by-skunk-acoustic-version-w-lap-steel",
   "???Sunflower": "sunflower",
   "???Superhero": "superhero-6-28-2020",
-  "The Nomads": "07-the-nomads",
-  "The Rain v. 5": "the-rain-5",
 };
 
 export const covers = {
-  "All My Loving": "all-my-loving",
   "Between the Devil and the Deep Blue Sea":
     "between-the-devil-and-the-deep-blue-sea",
   "City of Stars": "city-of-stars-steve",
