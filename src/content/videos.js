164e84bd87b70518b688f0945b404c834b44b212
Curtis Yungen <curtisyungen@gmail.com>
2021-11-13 19:12:54 -0800

    add videos.js to git

---
+++ src/content/videos.js
@@ -0,0 +1,16 @@
+const VIDEO = {
+    VIDEO_1: "https://www.youtube.com/embed/ftvl2ReR7rQ",
+    VIDEO_2: "https://www.youtube.com/embed/8W0qs8pDu64",
+    VIDEO_3: "https://www.youtube.com/embed/JOpgu_3O0WM",
+    VIDEO_4: "https://www.youtube.com/embed/8hw0KlIWbKk",
+    VIDEO_5: "https://www.youtube.com/embed/726hKa5MnAg",
+    VIDEO_6: "https://www.youtube.com/embed/AN5AX8W76Xc",
+    VIDEO_7: "https://www.youtube.com/embed/xEYHfnU0Z9o",
+    VIDEO_8: "https://www.youtube.com/embed/tgwyTx6I6sY",
+    VIDEO_9: "https://www.youtube.com/embed/ekyh20VfSoY",
+    VIDEO_10: "https://www.youtube.com/embed/pd9kunqRBQ8",
+    VIDEO_11: "https://www.youtube.com/embed/dFu8YgKDRJY",
+    VIDEO_12: "https://www.youtube.com/embed/vmlbEtXN-DQ",
+}
+
+export default VIDEO;
