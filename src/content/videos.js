abc9bc07ddc9d575f3334beb80e650abf4a14b41
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2024-09-22 10:21:01 -0700

    Update Videos

--- src/content/videos.js
+++ src/content/videos.js
@@ -1,10 +1,10 @@
 const VIDEO = {
     AT_THE_BEACH: "https://www.youtube.com/embed/xEYHfnU0Z9o", // At the Beach
-    AUG_14_2022: "https://www.youtube.com/embed/Z4r6bzcmIzQ", // Steve Yungen - Aug 14, 2022
+    AT_THE_BEACH_SEPT_2024: "https://www.youtube.com/embed/3eG975ft2jA?si=UEPu-VYpEhfTF685",
     BANANAS_THE_MOVIE: "https://www.youtube.com/embed/8W0qs8pDu64", // Bananas - THE MOVIE
-    FARMERS_MARKET_MAY_2022: "https://www.youtube.com/embed/ngCuCONHizQ", // Steve Yungen - Jacksonville, Oregon Farmers Market - May 15, 2022
+    BOMBAY_BEACH: "https://www.youtube.com/embed/a6O0xrpGdMw?si=3qrEnWCWCX5WZdza",
     HALLOWEEN_SONG: "https://www.youtube.com/embed/JOpgu_3O0WM", // Halloween Song
-    OCT_10_2021: "https://www.youtube.com/embed/2Jswsa9RwjE", // Steve Yungen - Oct 10, 2021
+    HONEYBEE: "https://www.youtube.com/embed/T4NVmcxGrmI?si=7iDYpSKUKB8D7OJ3",
     STEVE_JOSEPH_CURTIS_2019: "https://www.youtube.com/embed/ekyh20VfSoY", // Steve, Joseph and Curtis, 12/27/2019
     STEVE_JOSEPH_CURTIS_2022: "https://www.youtube.com/embed/ug_QohiXuKM", // Steve, Joseph & Curtis Yungen - August 7, 2022
     TWIN_CREEKS: "https://www.youtube.com/embed/726hKa5MnAg", // Steve Yungen at Twin Creeks park, Central Point, Oregon - Aug. 29, 2020
